764f7acee136a957ba7d928b5f9324f662ae9d82
psvfx <psvfxinvest@gmail.com>
2023-04-26 15:20:31 +0300

    Update lesson1_220423.js

--- js/lesson1_220423.js
+++ js/lesson1_220423.js
@@ -1,31 +1,30 @@
 // ЗАДАЧА 1
 // НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
 
-// const name = "ГАЛИНА";
-// const age = 35;
-// const lang = "JAVASCRIPT";
-
+// const userName = "ГАЛИНА";
+// const userAge = 35;
+// const userLang = "JAVASCRIPT";
 // console.log(
-//   `ДОБРОГО ДНЯ, МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${lang}`
+//   `ДОБРОГО ДНЯ, МЕНЕ ЗВАТИ ${userName}, МЕНІ ${userAge} РОКІВ, Я ВИВЧАЮ ${userLang}`
 // );
 
 // ЗАДАЧА 2
 // ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
 
-// const name = prompt("Enter your name");
-// const email = prompt("Enter your email");
-// const phone = prompt("Enter your phone");
-
+// const userName = prompt("Enter your name");
+// const userEmail = prompt("Enter your email");
+// const userPhone = prompt("Enter your phone");
 // console.log(
-//   `КОРИСТУВАЧ ${name} ВИКОРИСТОВУЄ ${email} ПОЧТУ І ${phone} ТЕЛЕФОН`
+//   `КОРИСТУВАЧ ${userName} ВИКОРИСТОВУЄ ${userEmail} ПОЧТУ І ${userPhone} ТЕЛЕФОН`
 // );
 
 // ЗАДАЧА 3
 // ПОРАХУЙТЕ СУМУ ТОВАРІВ У КОРЗИНІ . ПОТРІБНО ВВЕСТИ СУМУ 3-Х ТОВАРІВ І ПОРАХУВАТИ ЇХ СУМУ
-// const tvSet = Number(prompt('enter tvSet price'));
-// const tab = Number(prompt ('enter tab price'));
-// const phone = Number(prompt('enter phone price'));
-// const totalPrice = tvSet + tab + phone;
+
+// const priceTv = Number(prompt('Enter Tv price'));
+// const priceTab = Number(prompt ('Enter Tab price'));
+// const pricePhone = Number(prompt('Enter Phone price'));
+// const totalPrice = priceTv + priceTab + pricePhone;
 // console.log(totalPrice);
 
 // ЗАДАЧА 4
@@ -33,12 +32,12 @@
 // "СКІЛЬКИ МІСЯЦІВ МАЄ РІК", ЯКЩО КОРИСТУВАЧ ВВОДИТЬ 12 ТО У КОНСОЛІ ПИШЕ ВІРНО, ЯКЩО НЕ ПРАВИЛЬНО КОРИСТУВАЧ ВВІВ, ТОДІ У КОНСОЛІ ПИШЕ
 // "НЕ ЗНАЄТЕ? 12"
 
-// const month = Number(prompt("Enter month"));
-// if (month ===12) {
-//   console.log(`Вірно`);
+// const enterMonth = Number(prompt("Enter month"));
+// if (enterMonth ===12) {
+//   console.log('Вірно');
 // }else {
-//   console.log(`Не знаєте? 12`);}
-// const result = month === 12 ? "correct" :  'Не знаєте? 12';
+//   console.log('Не знаєте? 12');}
+// const result = enterMonth === 12 ? "correct" :  'Не знаєте? 12';
 // console.log(result);
 
 // ЗАДАЧА 5
@@ -71,14 +70,15 @@
 
 // ЗАДАЧА 7
 // НАПИШІТЬ ПРОГРАМУ, ЯКА ПРОВІРЯЄ ЛОГІН І ПАРОЛЬ, І ВИВОДИТЬ ПРИВІТАННЯ - ЯКЩО ВСЕ ДОБРЕ, АБО ВИДАЄ ПОМИЛКУ , ЩО НЕ ВІРНИЙ ЛОГІН
-const userLogEnter = prompt("Введіть ваш логін");
-const userPassEnter = prompt("Введіть ваш пароль");
-const userLog = "123ttt";
-const userPass = "qwer";
-if (userLogEnter === userLog && userPassEnter === userPass) {
-  console.log("ВІТАЮ! Ви здійснили вхід.");
-} else if (userLogEnter !== userLog) {
-  console.log("ПОМИЛКА! Ви ввели не вірний логін.");
-} else {
-  console.log("ПОМИЛКА! Ви ввели не вірний пароль.");
-}
+
+// const userLogEnter = prompt("Введіть ваш логін");
+// const userPassEnter = prompt("Введіть ваш пароль");
+// const userLog = "123ttt";
+// const userPass = "qwer";
+// if (userLogEnter === userLog && userPassEnter === userPass) {
+//   console.log("ВІТАЮ! Ви здійснили вхід.");
+// } else if (userLogEnter !== userLog) {
+//   console.log("ПОМИЛКА! Ви ввели не вірний логін.");
+// } else {
+//   console.log("ПОМИЛКА! Ви ввели не вірний пароль.");
+// }
